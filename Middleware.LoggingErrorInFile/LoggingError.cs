e8946321b56bf5122ce7b77760da15c53b83f6f9
Admin <Admin@LENOVO>
2023-05-15 15:09:42 +0600

    Добавьте файлы проекта.

---
+++ Middleware.LoggingErrorInFile/LoggingError.cs
@@ -0,0 +1,33 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Text;
+
+namespace Middleware.LoggingErrorInFile
+{
+    public class LoggingError
+    {
+        private readonly RequestDelegate _requestDelegate;
+
+        public LoggingError(RequestDelegate requestDelegate)
+        {
+            requestDelegate = requestDelegate ?? throw new ArgumentNullException(nameof(requestDelegate));
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                throw new Exception("Mistake occured while processing");
+                await _requestDelegate.Invoke(context);
+            }
+            catch (Exception e)
+            {
+                using (FileStream fstream = new FileStream("errors.txt", FileMode.OpenOrCreate))
+                {
+                    byte[] buffer = Encoding.Default.GetBytes(e.Message);
+                    
+                    await fstream.WriteAsync(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
---
+++ Middleware.LoggingErrorInFile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Middleware.LoggingErrorInFile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
